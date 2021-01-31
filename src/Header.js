d9f01c75f8f0cbb805244050231d799c310c90a0
Mateusz Stochel <mateuszstochel1995@gmail.com>
2021-01-31 16:45:44 +0100

    Add header jsx and styles

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header__title">ShopXxX</h1>
+      <div className="div header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Hello Guest</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCounter">0</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
